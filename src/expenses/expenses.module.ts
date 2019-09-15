fc6e2ba7235357d5c67d651bcc10f96d64766764
Tommy <tommyliu9@gmail.com>
2019-09-15 10:43:06 -0400

    fewfewf

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -3,14 +3,16 @@ import {MongooseModule} from '@nestjs/mongoose';
 import { ExpenseService } from './expenses.services';
 import { ExpenseController } from './expenses.controller';
 import { ExpensesSchema } from './expenses.schema';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
-  MongooseModule.forFeature([{ name: 'Expenses', schema: ExpensesSchema}]),
+  MongooseModule.forFeature([{ name: 'Expenses', schema: ExpensesSchema},]),
   HttpModule.register({
     timeout: 5000,
     maxRedirects: 5,
-  })
+  }),
+  ConfigModule
   ],
   controllers: [ExpenseController],
   providers: [ExpenseService],
